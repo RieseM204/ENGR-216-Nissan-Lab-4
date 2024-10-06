480d70e3859896b18dd6ab153aa7d9e7e069d5d0
RieseM204 <riese.marshall204@gmail.com>
2024-10-06 16:51:50 -0500

    yeah

--- run.py
+++ run.py
@@ -1,5 +1,29 @@
 """Main file, intended to be run"""
 
+#units are m, kg, s
+
+mass = 0.2241   # kg
+
+LO_HP = 0.178   # m
+G_YN = 0.217    # m
+
+A_r = 0.00378   # m^2
+A_w = 0.00364   # m^2
+
+def static(material : str):
+    """Does all the static friction stuff"""
+    
+    raise NotImplementedError
+
+def kinetic(material : str):
+    """Does all the kinetic friction stuff"""
+    
+    raise NotImplementedError
+
+def main():
+    """Everything all together"""
+
+    raise NotImplementedError
 
 if __name__ == "__main__":
-    pass
+    main()
