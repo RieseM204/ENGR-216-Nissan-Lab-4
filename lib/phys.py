12459a42064f8458edda04d3f04e8710f5f8087b
Lily <riese.marshall204@gmail.com>
2024-10-06 23:17:47 -0500

    Added a bunch of commenting

--- lib/phys.py
+++ lib/phys.py
@@ -1,5 +1,4 @@
 """Library for working with forces in order to solve classical mechanics problems"""
-# WIP
 
 import numpy as np
 import pandas as pd
@@ -7,10 +6,10 @@ import matplotlib.pyplot as plt
 import statistics as stat
 import math
 
-def calc_fric_k(theta, a):
-    g = 9.81
-    coeff = math.tan(theta) - (a)/(g * math.cos(theta))
-    return coeff
+def calc_fric_k(theta, a):                                  # Take the angle and measure acceleration as inputs
+    g = 9.81                                                # Acceleration due to gravity in m/s^2
+    coeff = math.tan(theta) - (a)/(g * math.cos(theta))     # Calculate the coefficient using my derrived equation
+    return coeff                                            # Return the coefficient
 
-def calc_fric_s(theta):
-    return math.tan(theta)
+def calc_fric_s(theta):                                     # Take the minimum angle at which the block accelerates as input
+    return math.tan(theta)                                  # Return the tangent of the angle (it really is that simple)
